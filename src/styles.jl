208f1a3c2fae51a670e9c843c02c6576b96976c5
Rafael <rafaelab@users.noreply.github.com>
2026-03-04 14:33:28 +0100

    organise styles file

---
+++ src/styles.jl
@@ -0,0 +1,13 @@
+# ----------------------------------------------------------------------------------------------- #
+#
+include(joinpath("styles", "plain.jl"))
+include(joinpath("styles", "unsrt.jl"))
+include(joinpath("styles", "alpha.jl"))
+include(joinpath("styles", "ieeestr.jl"))
+include(joinpath("styles", "revtex.jl"))
+include(joinpath("styles", "jhep.jl"))
+include(joinpath("styles", "full.jl"))
+include(joinpath("styles", "abntex2-num.jl"))
+include(joinpath("styles", "abntex2-alpha.jl"))
+
+# ----------------------------------------------------------------------------------------------- #
