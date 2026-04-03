8d45f2e778237362eb6ff3f0276bba96f0bef069
Rafael <rafaelab@users.noreply.github.com>
2026-04-03 10:06:39 +0200

    rename files

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,8 +7,8 @@ using PythonCall: pyconvert
 
 include("common.jl")
 include("crossref.jl")
-include("json_io.jl")
-include("yaml_io.jl")
+include("jsonIO.jl")
+include("yamlIO.jl")
 include("entry.jl")
 include("library.jl")
 include("aux.jl")
