50593545428513a54a32922c508d1ab4d889d8ae
Rafael <rafaelab@users.noreply.github.com>
2026-04-04 18:15:51 +0200

    minor style changes

--- cli/buildExecutable.jl
+++ cli/buildExecutable.jl
@@ -8,22 +8,20 @@ Pkg.instantiate()
 
 
 const exeName = Sys.iswindows() ? "zettel.exe" : "zettel"
-const exeDir = joinpath(@__DIR__, "..", "lib")
+const exeDir = joinpath(@__DIR__, "..", "bin")
 const exePath = joinpath(exeDir, exeName)
 const entrypoint = joinpath(@__DIR__, "entrypoint.jl")
 
-mkpath(exeDir)
 
 extraFlags = String.(split(get(ENV, "JULIAC_FLAGS", "")))
-args = String.(vcat(
-	["--project", abspath(@__DIR__), "--output-exe", exeName, abspath(entrypoint)],
-	extraFlags,
-))
+args = String.(vcat(["--project", abspath(@__DIR__), "--output-exe", exeName, abspath(entrypoint)], extraFlags))
 
 @info "building executable; writing to: $exePath"
 cd(exeDir) do
 	JuliaC.main(args)
 end
 
-isfile(exePath) || error("Executable was not produced at $exePath")
+if ! isfile(exePath)
+	throw(ErrorException("Executable was not produced at $(exePath)."))
+end
 @info "✓ executable created successfully"
