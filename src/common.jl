b688f8055d1abd867bea3923706d290be73c94f5
Rafael <rafaelab@users.noreply.github.com>
2026-04-04 17:13:25 +0200

    bug fixes in CLI

--- src/common.jl
+++ src/common.jl
@@ -2,4 +2,24 @@
 #
 const Maybe = Union{Nothing, T} where T
 
+
+# ----------------------------------------------------------------------------------------------- #
+#
+@doc """
+	_getFileExtension(filename)
+
+Identify the bibliography format of a file based on its extension.
+
+# Input
+- `filename::AbstractString`: path to the file whose format is to be identified.
+
+# Output
+- A string corresponding to the file's extension, in lowercase and with leading/trailing whitespace removed.
+"""
+function _getFileExtension(filename::AbstractString)
+	return lowercase(strip(splitext(filename)[2]))
+end
+
+
+
 # ----------------------------------------------------------------------------------------------- #
